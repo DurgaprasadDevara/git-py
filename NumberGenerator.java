6f0ef214bf472fd7d9317ffa286dedf7fe1796e2
DurgaprasadDevara <durgaprasad.devara120218@gmail.com>
2024-01-11 19:45:05 +0530

    Added NumberGenerator class

---
+++ NumberGenerator.java
@@ -0,0 +1,23 @@
+public class NumberGenerator {
+	private int startNum;
+	private int endNum;
+
+	public NumberGenerator(int a, int b) {
+		this.startNum = startNum;
+		this.endNum = endNum;
+	}
+
+	public void generator() {
+		Thread curTh = Thread.currentThread();
+		String thName = curTh.getName();
+		System.out.println("Generating sequence of numbers from " + startNum + " to " + endNum);
+		for (int i = startNum; i <= endNum; i++) {
+			System.out.println(thName + " i = " + i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
